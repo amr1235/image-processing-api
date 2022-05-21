a64978a4204c350507893c35ab1162a09c75bbc3
amr1235 <amr.ali99@eng-st.cu.edu.eg>
2022-05-21 12:59:35 +0200

    API done without testing

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import express from 'express';
+import apicache from 'apicache';
 import MainRout from './routes/index';
 import ImagesRoute from './routes/images';
-
 const PORT = 3000;
 const app = express();
 
 // routes setup
 app.use('/api', MainRout);
 app.use('/api/images', ImagesRoute);
+
+// add apicache middleware for chacheing
+const cache = apicache.middleware;
+//caching all routes for 5 minutes
+app.use(cache('5 minutes'));
 
 app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`);
--- src/routes/images.ts
+++ src/routes/images.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import { resize } from '../lib/image';
 
 const images = express.Router();
@@ -6,7 +7,29 @@ const images = express.Router();
 images.get(
   '/',
   async (req: express.Request, res: express.Response): Promise<void> => {
-    res.send('images route test');
+    // get the parameters in the query string
+    const fileName: string = req.query.filename as string;
+    const width: number = parseInt(req.query.width as string);
+    const height: number = parseInt(req.query.height as string);
+    // check if there any missing parameters
+    if (!fileName || !width || !height) {
+      //client error
+      res.status(400).send(`<h1>Missing or Inavlid Parameters</h1></br>
+        please make sure you enter the parameters as follows : ?filename={file_name:string}&width={width:number}&height={height:number}`);
+      return;
+    }
+    const inputImage =
+      path.resolve('./assests/full/') + '/' + fileName + '.jpg';
+    const outputImage =
+      path.resolve('./assests/thump/') + '/' + fileName + '.jpg';
+    // resize the image
+    try {
+      await resize(inputImage, width, height, outputImage);
+      res.status(200).sendFile(outputImage);
+      return;
+    } catch (err) {
+      res.status(500).send(err);
+    }
   }
 );
 
